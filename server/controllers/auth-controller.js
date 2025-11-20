c0b227b0bbfd56f738044ca4252fd2b8e5f9dbe5
heinzarniaung <heinzarni.aung@stonybrook.edu>
2025-11-20 17:14:58 -0500

    Changed auth controller so that it uses a random UUID rather than db id.

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,21 +1,22 @@
 const auth = require('../auth')
-const db = require('../db')
+const User = require('../models/user-model')
 const bcrypt = require('bcryptjs')
+const { randomUUID } = require('crypto');
+
 
 getLoggedIn = async (req, res) => {
     try {
         let userId = auth.verifyUser(req);
-        console.log(`userId from token: ${userId}`)
         if (!userId) {
-            console.log("No userId, returning loggedIn: false");
             return res.status(200).json({
                 loggedIn: false,
                 user: null,
-                errorMessage: "No userId"
+                errorMessage: "?"
             })
         }
 
         const loggedInUser = await User.findOne({ _id: userId });
+        console.log("loggedInUser: " + loggedInUser);
 
         return res.status(200).json({
             loggedIn: true,
@@ -31,107 +32,42 @@ getLoggedIn = async (req, res) => {
     }
 }
 
-registerUser = async (req, res) => {
-    console.log("NOW REGISTERING USER IN BACKEND")
-    try {
-        const { firstName, lastName, email, password, passwordVerify } = req.body;
-        console.log(`Creating user: ${firstName} ${lastName} ${email} ${password} ${passwordVerify}`);
-        // If any of these fields are empty, return error.
-        if (!firstName || !lastName || !email || !password || !passwordVerify) {
-            return res
-                    .status(400)
-                    .json({ errorMessage: "Please enter all required fields."});
-        }
-        // If password has insufficient length, return error.
-        if (password.length < 8) {
-            return res
-                    .status(400)
-                    .json({ errorMessage: "Password must be at least 8 characters."})
-        }
-        if (password !== passwordVerify) {
-            return res
-                    .status(400)
-                    .json({ errorMessage: "Passwords do not match!"})
-        }
-        // check if an existing user already exists.
-        const existingUser = await db.findUserByEmail(email);
-        if (existingUser) {
-            return res
-                    .status(400)
-                    .json({
-                        success: false,
-                        errorMessage: "An account with this email address already exists."
-                    })
-        }
-
-        // Creating password Hash
-        const saltRounds = 10;
-        const salt = await bcrypt.genSalt(saltRounds);
-        const passwordHash = await bcrypt.hash(password, salt);
-        console.log("passwordHash: " + passwordHash)
-
-        // Saving the new user
-        const savedUser = await db.createUser({
-            firstName,
-            lastName,
-            email,
-            passwordHash
-        })
-        console.log(`New user saved: ${savedUser._id}`);
-
-        // LOGIN THE USER
-        const userId = savedUser._id;
-        const token = auth.signToken(userId);
-        // sending the cookie back in response 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            samesite: 'lax'
-        }).status(200).json({
-            success: true,
-            user: {
-                firstName: savedUser.firstName,
-                lastName: savedUser.lastName,
-                email: savedUser.email
-            }
-        })
-        console.log(`Token has been sent!`)
-    } catch (error) {
-        console.error("Error in registered User: ", err);
-        res.status(500).json({ errorMessage: "Server error: " + err.message });
-    }
-}
-
 loginUser = async (req, res) => {
+    console.log("loginUser");
     try {
         const { email, password } = req.body;
+
         if (!email || !password) {
             return res
-                    .status(400)
-                    .json({ error: "Please enter all required fields."});
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
         }
 
         const existingUser = await User.findOne({ email: email });
         console.log("existingUser: " + existingUser);
         if (!existingUser) {
             return res
-                    .status(401)
-                    .json({
-                        errorMessage: "Wrong email or password provided or user does not exist."
-                    })
-        }
-        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
-        if (!passwordCorrect) {
-            return res
-                    .status(401)
-                    .json({
-                        errorMessage: "Wrong email or password provided."
-                    })
+                .status(401)
+                .json({
+                    errorMessage: "Wrong email or password provided."
+                })
         }
 
-        // LOGIN USER
+        console.log("provided password: " + password);
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+        if (!passwordCorrect) {
+            console.log("Incorrect password");
+            return res
+                .status(401)
+                .json({
+                    errorMessage: "Wrong email or password provided."
+                })
+        }
+
+        // LOGIN THE USER
         const token = auth.signToken(existingUser._id);
-        
+        console.log(token);
+
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
@@ -140,10 +76,11 @@ loginUser = async (req, res) => {
             success: true,
             user: {
                 firstName: existingUser.firstName,
-                lastName: existingUser.lastName,
-                email: existingUser.email
+                lastName: existingUser.lastName,  
+                email: existingUser.email              
             }
         })
+
     } catch (err) {
         console.error(err);
         res.status(500).send();
@@ -157,6 +94,78 @@ logoutUser = async (req, res) => {
         secure: true,
         sameSite: "none"
     }).send();
+}
+
+registerUser = async (req, res) => {
+    console.log("REGISTERING USER IN BACKEND");
+    try {
+        const { firstName, lastName, email, password, passwordVerify } = req.body;
+        console.log("create user: " + firstName + " " + lastName + " " + email + " " + password + " " + passwordVerify);
+        if (!firstName || !lastName || !email || !password || !passwordVerify) {
+            return res
+                .status(400)
+                .json({ errorMessage: "Please enter all required fields." });
+        }
+        console.log("all fields provided");
+        if (password.length < 8) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter a password of at least 8 characters."
+                });
+        }
+        console.log("password long enough");
+        if (password !== passwordVerify) {
+            return res
+                .status(400)
+                .json({
+                    errorMessage: "Please enter the same password twice."
+                })
+        }
+        console.log("password and password verify match");
+        const existingUser = await User.findOne({ email: email });
+        console.log("existingUser: " + existingUser);
+        if (existingUser) {
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    errorMessage: "An account with this email address already exists."
+                })
+        }
+
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const passwordHash = await bcrypt.hash(password, salt);
+        console.log("passwordHash: " + passwordHash);
+
+        const newUser = new User({firstName, lastName, email, passwordHash});
+        const savedUser = await newUser.save();
+        console.log("new user saved: " + savedUser._id);
+
+        // LOGIN THE USER
+        const token = auth.signToken(savedUser._id);
+        console.log("token:" + token);
+
+        await res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }).status(200).json({
+            success: true,
+            user: {
+                firstName: savedUser.firstName,
+                lastName: savedUser.lastName,  
+                email: savedUser.email              
+            }
+        })
+
+        console.log("token sent");
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
 }
 
 module.exports = {
