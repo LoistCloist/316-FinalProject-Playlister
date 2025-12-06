358ab6995eb4132a881ae4bc083c059e5958dec2
heinzarniaung <heinzarni.aung@stonybrook.edu>
2025-12-06 18:44:01 -0500

    Client store requests written.

---
+++ client/src/stores/requests/playlistRequestSender.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+axios.defaults.withCredentials = true;
+const api = axios.create({
+    baseUrl: 'http://localhost:4000/store',
+})
+
+export const createPlaylist = (playlistName, userName, email, songs) => {
+    return api.post('/playlist/', {
+        playlistName: playlistName,
+        userName: userName,
+        email: email,
+        songs: songs,
+    })
+}
+export const deletePlaylistById = (id) => {
+    return api.delete(`/playlist/${id}`);
+}
+export const getPlaylists = () => {
+    return api.get('/playlist/');
+}
+export const getPlaylistById = (id) => {
+    return api.get(`/playlist/${id}`)
+}   
+export const getUserPlaylists = (userid) => {
+    return api.get(`userplaylist/${userid}`)
+}
+export const getAllPlaylists = () => {
+    return api.get('/allplaylists')
+}
+export const updatePlaylist = (id, playlistName, userName, email, songs) => {
+    return api.put(`/playlist/${id}`, {
+        playlistName: playlistName,
+        userName: userName,
+        email: email,
+        songs: songs,
+    })
+}
+
+const apis = {
+    createPlaylist,
+    deletePlaylistById,
+    getPlaylists,
+    getPlaylistById,
+    getUserPlaylists,
+    getAllPlaylists,
+    updatePlaylist
+}
+
+export default apis;
---
+++ client/src/stores/requests/songRequestSender.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+axios.defaults.withCredentials = true;
+const api = axios.create({
+    baseUrl: 'http://localhost:4000/store',
+})
+
+export const createSong = ( title, artist, year, youtubeId, ownerEmail ) => {
+    return api.post('/songs/', {
+        title: title,
+        artist: artist,
+        year: year,
+        youtubeId: youtubeId,
+        ownerEmail: ownerEmail
+    })
+}
+export const getTargetSongs = (id) => {
+    return api.get(`/songs/${id}`);
+}
+export const getSongById = (id) => {
+    return api.get(`/songs/${id}`);
+}
+export const editSongById = ( songId, title, artist, year, youtubeId ) => {
+    return api.put(`/songs/${songId}`, {
+        title: title,
+        artist: artist,
+        year: year,
+        youtubeId: youtubeId
+    })
+}   
+export const getAllSongsInPlaylist = (userid) => {
+    return api.get(`userplaylist/${userid}`)
+}
+export const getUserSongs = () => {
+    return api.get(`/songs/all`)
+}
+export const deleteSongById = (id) => {
+    return api.delete(`/playlist/${id}`)
+}
+
+const apis = {
+    createSong,
+    getTargetSongs,
+    getSongById,
+    editSongById,
+    getAllSongsInPlaylist,
+    getUserSongs,
+    deleteSongById
+}
+
+export default apis
